07c72b6f6fcc3a1402b4ef7ec9525385db1eb8cd
Thomas Rogers <thomasrogers03@gmail.com>
2020-11-13 09:24:35 -0500

    setup for tests

---
+++ bloom/tests/test_sector_split.py
@@ -0,0 +1,9 @@
+# Copyright 2020 Thomas Rogers
+# SPDX-License-Identifier: Apache-2.0
+
+import unittest
+
+class TestSectorSplit(unittest.TestCase):
+
+    def test_can_split(self):
+        pass
--- build_and_release.sh
+++ build_and_release.sh
@@ -2,9 +2,5 @@
 
 set -eou pipefail
 
-rm -rf dist/*
-
-python setup.py bdist_apps
-python setup.py bdist_wheel
-
+./build.sh
 ./release.sh
