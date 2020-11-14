c54d0dcfc13369bfe4bec41e69f71e55f24441c9
Thomas Rogers <thomasrogers03@gmail.com>
2020-11-14 14:02:26 -0500

    test for other side wall delete

--- bloom/tests/test_wall_delete.py
+++ bloom/tests/test_wall_delete.py
@@ -61,3 +61,17 @@ class TestWallDelete(unittest.TestCase):
         wall = utils.find_wall_on_point(sector, core.Point2(1, 1))
         self.assertEqual(wall.point_2, core.Point2(-1, 1))
         self.assertEqual(wall.other_side_wall.point_1, core.Point2(-1, 1))
+
+    @unittest.skip
+    def test_can_delete_with_other_side(self):
+        sector = utils.build_rectangular_sector(self._sectors, -1, 1, -1, 1)
+        other_side_sector = utils.build_rectangular_sector(self._sectors, -1, 1, 1, 2)
+
+        wall = utils.find_wall_on_point(sector, core.Point2(1, 1))
+        operations.wall_link.SectorWallLink(wall, self._sectors).try_link_wall()
+
+        other_side_wall = utils.find_wall_on_point(other_side_sector, core.Point2(-1, 1))
+        operations.wall_delete.WallDelete(other_side_wall).delete()
+
+        wall = utils.find_wall_on_point(sector, core.Point2(1, 1))
+        self.assertEqual(wall.wall_point_2.point_1, core.Point2(-1, 2))
