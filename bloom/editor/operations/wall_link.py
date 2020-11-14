14fed147fccd363333ff90b71a9a2dd857bee952
Thomas Rogers <thomasrogers03@gmail.com>
2020-11-14 12:26:08 -0500

    wall link fixes

--- bloom/editor/operations/wall_link.py
+++ bloom/editor/operations/wall_link.py
@@ -48,8 +48,9 @@ class SectorWallLink:
                     test_wall = test_wall.wall_point_2
             else:
                 wall_split.WallSplit(test_wall).split(overlap.point_1)
-                wall_split.WallSplit(test_wall).split(overlap.point_2)
-                test_wall = test_wall.wall_point_2
+                if test_wall.point_1 != overlap.point_2:
+                    wall_split.WallSplit(test_wall).split(overlap.point_2)
+                    test_wall = test_wall.wall_point_2
             test_wall._other_side_wall
             test_wall.link(wall_to_link)
             return True
