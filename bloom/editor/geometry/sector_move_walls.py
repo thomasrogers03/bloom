cfb20953c72238c5c2b44da8407649b6e255b965
Thomas Rogers <thomasrogers03@gmail.com>
2020-12-06 12:59:04 -0500

    move sprites too

--- bloom/editor/geometry/sector_move_walls.py
+++ bloom/editor/geometry/sector_move_walls.py
@@ -13,13 +13,22 @@ class SectorMoveWalls(empty_move.EmptyMove):
 
     def __init__(self, sector: map_objects.EditorSector):
         self._sector = sector
-        self._start_points = [wall.point_1 for wall in self._sector.walls]
+        self._wall_start_points = [wall.point_1 for wall in self._sector.walls]
+        self._sprite_start_points = [sprite.origin for sprite in self._sector.sprites]
 
     def get_move_direction(self):
         return core.Vec3(0, 0, 0)
 
     def move(self, move_delta: core.Vec3, snapper: grid_snapper.GridSnapper):
         move_delta_2d = snapper.snap_to_grid_2d(move_delta.xy)
-        for wall, start_point in zip(self._sector.walls, self._start_points):
+        for wall, start_point in zip(self._sector.walls, self._wall_start_points):
             new_position = start_point + move_delta_2d
             wall.teleport_point_1_to(new_position)
+
+        for sprite, start_point in zip(self._sector.sprites, self._sprite_start_points):
+            new_position = core.Point3(
+                start_point.x + move_delta_2d.x,
+                start_point.y + move_delta_2d.y,
+                start_point.z
+            )
+            sprite.move_to(new_position)
