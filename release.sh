b9919e5c09543edc03884121b23becdd433090a6
Thomas Rogers <thomasrogers03@gmail.com>
2020-12-21 09:54:23 -0500

    auto build as draft

--- release.sh
+++ release.sh
@@ -7,4 +7,4 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 sha=$(git rev-parse HEAD)
 
 echo "Creating release $branch-$version"
-ghr -t $GITHUB_TOKEN -u thomasrogers03 -r bloom -c $sha -delete "$branch-$version" ./dist/
+echo ghr -t $GITHUB_TOKEN -u thomasrogers03 -r bloom -c $sha -draft -replace "$branch-$version" ./dist/
