6542895bb81ee52c9b329590573cfe0f131669ae
Thomas Rogers <thomasrogers03@gmail.com>
2020-12-21 09:59:21 -0500

    fix release

--- release.sh
+++ release.sh
@@ -7,4 +7,4 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 sha=$(git rev-parse HEAD)
 
 echo "Creating release $branch-$version"
-echo ghr -t $GITHUB_TOKEN -u thomasrogers03 -r bloom -c $sha -draft -replace "$branch-$version" ./dist/
+ghr -t $GITHUB_TOKEN -u thomasrogers03 -r bloom -c $sha -prerelease -draft -replace "$branch-$version" ./dist/
