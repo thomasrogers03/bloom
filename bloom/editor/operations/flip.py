11b0dd03926f0278af23de3c267769ba771ad685
Thomas Rogers <thomasrogers03@gmail.com>
2020-12-30 13:06:03 -0500

    refactor

--- bloom/editor/operations/flip.py
+++ bloom/editor/operations/flip.py
@@ -2,47 +2,36 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .. import map_objects
-from .undo_stack import SimplyUndoableOperation, UndoStack
+from .undo_stack import SimpleUndoableOperation, UndoStack
+
 
 class Flip:
 
     def __init__(self, undo_stack: UndoStack, map_object: map_objects.EmptyObject, part: str):
         self._undo_stack = undo_stack
         self._map_object = map_object
-        self._stat = map_object.get_stat_for_part(part)
+        self._part = part
 
     def flip(self):
         self._map_object.invalidate_geometry()
 
-        previous_xflip = self._stat.xflip
-        previous_yflip = self._stat.yflip
-        
-        if self._stat.xflip and self._stat.yflip:
-            self._stat.xflip = 0
-        elif self._stat.xflip:
-            self._stat.xflip = 1
-            self._stat.yflip = 1
-        elif self._stat.yflip:
-            self._stat.yflip = 0
-        else:
-            self._stat.xflip = 1
+        with self._undo_stack.property_change(
+            'Sprite/Wall Flip',
+            self._map_object,
+            self._part,
+            'xflip',
+            'yflip'
+        ):
+            if self._stat.xflip and self._stat.yflip:
+                self._stat.xflip = 0
+            elif self._stat.xflip:
+                self._stat.xflip = 1
+                self._stat.yflip = 1
+            elif self._stat.yflip:
+                self._stat.yflip = 0
+            else:
+                self._stat.xflip = 1
 
-        new_xflip = self._stat.xflip
-        new_yflip = self._stat.yflip
-
-        def _undo():
-            self._map_object.invalidate_geometry()
-            self._stat.xflip = previous_xflip
-            self._stat.yflip = previous_yflip
-
-        def _redo():
-            self._map_object.invalidate_geometry()
-            self._stat.xflip = new_xflip
-            self._stat.yflip = new_yflip
-
-        operation = SimplyUndoableOperation(
-            'Sprite/Wall Flip', 
-            _undo,
-            _redo
-        )
-        self._undo_stack.add_operation(operation)
+    @property
+    def _stat(self):
+        return self._map_object.get_stat_for_part(self._part)
--- bloom/editor/operations/undo_stack.py
+++ bloom/editor/operations/undo_stack.py
@@ -4,6 +4,8 @@
 import typing
 
 from ... import cameras
+from .. import map_objects
+from contextlib import contextmanager
 
 
 class UndoableOperation:
@@ -18,7 +20,7 @@ class UndoableOperation:
         raise NotImplementedError()
 
 
-class SimplyUndoableOperation:
+class SimpleUndoableOperation:
 
     def __init__(self, name: str, undo, redo):
         self._name = name
@@ -27,6 +29,31 @@ class SimplyUndoableOperation:
 
     def get_name(self):
         return self._name
+
+
+@contextmanager
+def _property_change(name: str, map_object: map_objects.EmptyObject, part: str, property_names: typing.List[str]):
+    old_values: typing.Dict[str, typing.Any] = {}
+    new_values: typing.Dict[str, typing.Any] = {}
+    stat = map_object.get_stat_for_part(part)
+
+    for property_name in property_names:
+        old_values[property_name] = getattr(stat, property_name)
+
+    def _undo():
+        map_object.invalidate_geometry()
+        for property_name in property_names:
+            setattr(stat, property_name, old_values[property_name])
+
+    def _redo():
+        map_object.invalidate_geometry()
+        for property_name in property_names:
+            setattr(stat, property_name, new_values[property_name])
+
+    yield SimpleUndoableOperation(name, _undo, _redo)
+
+    for property_name in property_names:
+        new_values[property_name] = getattr(stat, property_name)
 
 
 class UndoStack:
@@ -59,3 +86,15 @@ class UndoStack:
 
         self._camera_collection.set_info_text(f'Redo: {operation.get_name()}')
         self._undo.append(operation)
+
+    @contextmanager
+    def property_change(
+        self,
+        name: str,
+        map_object: map_objects.EmptyObject,
+        part: str,
+        *property_names: typing.List[str]
+    ):
+        with _property_change(name, map_object, part, property_names) as operation:
+            yield
+            self.add_operation(operation)
