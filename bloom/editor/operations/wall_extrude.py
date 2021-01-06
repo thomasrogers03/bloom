baea505e14c1e3eb4805a33847d9ce88f6db196c
Thomas Rogers <thomasrogers03@gmail.com>
2021-01-06 09:01:30 -0500

    wall extrude undo

--- bloom/editor/operations/wall_extrude.py
+++ bloom/editor/operations/wall_extrude.py
@@ -19,62 +19,63 @@ class WallExtrude:
         if self._wall.other_side_wall is not None:
             return
 
-        new_sector = self._all_sectors.create_sector(self._sector)
-        extrude_direction = self._wall.get_normal() * 2048
+        with self._wall.undos.multi_step_undo('Wall Extrude'):
+            new_sector = self._all_sectors.create_sector(self._sector)
+            extrude_direction = self._wall.get_normal() * 2048
 
-        blood_point_1 = self._blood_wall.copy()
-        blood_point_1.wall.position_x = self._blood_wall_point_2.wall.position_x
-        blood_point_1.wall.position_y = self._blood_wall_point_2.wall.position_y
-        point_1 = new_sector.add_wall(blood_point_1)
+            blood_point_1 = self._blood_wall.copy()
+            blood_point_1.wall.position_x = self._blood_wall_point_2.wall.position_x
+            blood_point_1.wall.position_y = self._blood_wall_point_2.wall.position_y
+            point_1 = new_sector.add_wall(blood_point_1)
 
-        blood_point_2 = self._blood_wall.copy()
-        blood_point_2.wall.position_x = self._blood_wall.wall.position_x
-        blood_point_2.wall.position_y = self._blood_wall.wall.position_y
-        point_2 = new_sector.add_wall(blood_point_2)
+            blood_point_2 = self._blood_wall.copy()
+            blood_point_2.wall.position_x = self._blood_wall.wall.position_x
+            blood_point_2.wall.position_y = self._blood_wall.wall.position_y
+            point_2 = new_sector.add_wall(blood_point_2)
 
-        blood_point_3 = self._blood_wall.copy()
-        blood_point_3.wall.position_x = int(
-            self._blood_wall.wall.position_x + extrude_direction.x
-        )
-        blood_point_3.wall.position_y = int(
-            self._blood_wall.wall.position_y + extrude_direction.y
-        )
-        point_3 = new_sector.add_wall(blood_point_3)
-
-        blood_point_4 = self._blood_wall.copy()
-        blood_point_4.wall.position_x = int(
-            self._blood_wall_point_2.wall.position_x + extrude_direction.x
-        )
-        blood_point_4.wall.position_y = int(
-            self._blood_wall_point_2.wall.position_y + extrude_direction.y
-        )
-        point_4 = new_sector.add_wall(blood_point_4)
-
-        self._wall.link(point_1)
-
-        point_1.wall_previous_point = point_4
-        point_2.wall_previous_point = point_1
-        point_3.wall_previous_point = point_2
-        point_4.wall_previous_point = point_3
-
-        new_segments = [
-            (point_1, point_2),
-            (point_2, point_3),
-            (point_3, point_4),
-            (point_4, point_1)
-        ]
-        for new_wall, new_wall_point_2 in new_segments:
-            new_wall.setup(
-                new_wall_point_2,
-                None
+            blood_point_3 = self._blood_wall.copy()
+            blood_point_3.wall.position_x = int(
+                self._blood_wall.wall.position_x + extrude_direction.x
             )
-            new_wall.reset_panning_and_repeats(None)
-        point_1.link(self._wall)
+            blood_point_3.wall.position_y = int(
+                self._blood_wall.wall.position_y + extrude_direction.y
+            )
+            point_3 = new_sector.add_wall(blood_point_3)
 
-        for new_wall in [point_2, point_3, point_4]:
-            wall_link.SectorWallLink(new_wall, self._all_sectors).try_link_wall()
+            blood_point_4 = self._blood_wall.copy()
+            blood_point_4.wall.position_x = int(
+                self._blood_wall_point_2.wall.position_x + extrude_direction.x
+            )
+            blood_point_4.wall.position_y = int(
+                self._blood_wall_point_2.wall.position_y + extrude_direction.y
+            )
+            point_4 = new_sector.add_wall(blood_point_4)
 
-        self._wall.invalidate_geometry()
+            self._wall.link(point_1)
+
+            point_1.wall_previous_point = point_4
+            point_2.wall_previous_point = point_1
+            point_3.wall_previous_point = point_2
+            point_4.wall_previous_point = point_3
+
+            new_segments = [
+                (point_1, point_2),
+                (point_2, point_3),
+                (point_3, point_4),
+                (point_4, point_1)
+            ]
+            for new_wall, new_wall_point_2 in new_segments:
+                new_wall.setup(
+                    new_wall_point_2,
+                    None
+                )
+                new_wall.reset_panning_and_repeats(None)
+            point_1.link(self._wall)
+
+            for new_wall in [point_2, point_3, point_4]:
+                wall_link.SectorWallLink(new_wall, self._all_sectors).try_link_wall()
+
+            self._wall.invalidate_geometry()
 
     @property
     def _sector(self) -> map_objects.EditorSector:
