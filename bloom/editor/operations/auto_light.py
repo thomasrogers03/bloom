e1081dd8dc74bc7786d1a3b711883c52165caea4
Thomas Rogers <thomasrogers03@gmail.com>
2021-01-10 18:01:57 -0500

    more lights

--- bloom/editor/operations/auto_light.py
+++ bloom/editor/operations/auto_light.py
@@ -2,13 +2,23 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import typing
-
-from ... import constants
-from .. import map_objects
 from concurrent import futures
+
+import yaml
+
+from ... import constants, find_resource
+from .. import map_objects
+
+
+def _load_light_types():
+    path = find_resource('light_tiles.yaml')
+    with open(path, 'r') as file:
+        result = yaml.safe_load(file.read())
+    return {key: set(value) for key, value in result.items()}
 
 
 class AutoLight:
+    _light_types = _load_light_types()
     _executor = futures.ThreadPoolExecutor(max_workers=10)
 
     def __init__(self, all_sectors: map_objects.SectorCollection):
@@ -19,7 +29,7 @@ class AutoLight:
         for sector in self._non_sky_sectors():
             future = self._executor.submit(self._apply_shade, sector)
             futures.append(future)
-        
+
         for future in futures:
             future.result()
 
@@ -43,8 +53,7 @@ class AutoLight:
         depth: int,
         seen: typing.Set[map_objects.EditorSector]
     ):
-        stat = sector.get_stat_for_part(map_objects.EditorSector.CEILING_PART)
-        if stat.parallax or depth > 4 or depth >= distance or sector in seen:
+        if self._sector_has_light(sector) or depth > 4 or depth >= distance or sector in seen:
             return depth
 
         for portal in sector.portal_walls():
@@ -68,6 +77,19 @@ class AutoLight:
                 distance = new_distance
 
         return distance
+
+    def _sector_has_light(self, sector: map_objects.EditorSector):
+        stat = sector.get_stat_for_part(map_objects.EditorSector.CEILING_PART)
+        return stat.parallax or \
+            sector.get_picnum(map_objects.EditorSector.FLOOR_PART) in self._light_types['floors'] or \
+            sector.get_picnum(map_objects.EditorSector.CEILING_PART) in self._light_types['ceilings'] or \
+            self._sector_has_light_sprite(sector)
+
+    def _sector_has_light_sprite(self, sector: map_objects.EditorSector):
+        for sprite in sector.sprites:
+            if sprite.get_picnum(None) in self._light_types['sprites']:
+                return True
+        return False
 
     def _non_sky_sectors(self):
         for sector in self._all_sectors.sectors:
