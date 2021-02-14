ccd13533cd524b4219b54bf636813875807aed13
Thomas Rogers <thomasrogers03@gmail.com>
2021-02-13 23:40:19 -0500

    first iteration of ROR wall moves

--- bloom/editor/geometry/wall_move.py
+++ bloom/editor/geometry/wall_move.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+import typing
 from panda3d import core
 
 from .. import grid_snapper, map_objects
@@ -44,12 +45,42 @@ class WallMove(empty_move.EmptyMove):
     def _move_vertex(wall: map_objects.EditorWall, new_position: core.Point2):
         walls_to_move = wall.all_walls_at_point_1()
         for point in walls_to_move:
-            repeat_adjust_previous_point = wall_repeat_adjust.WallRepeatAdjust(
-                point.wall_previous_point
-            )
-            repeat_adjust = wall_repeat_adjust.WallRepeatAdjust(point)
+            vertex_moves: typing.List[typing.Tuple[map_objects.EditorWall, core.Point2]] = [
+                (point, new_position)
+            ]
 
-            point.teleport_point_1_to(new_position)
+            point_sector: map_objects.EditorSector = point.sector
+            sector_above = point_sector.sector_above_ceiling
+            wall_above = point
+            while sector_above is not None:
+                offset: core.Vec3 = sector_above.get_above_draw_offset()
+                wall_above = sector_above.find_wall_on_point(wall_above.point_1 + offset.xy)
+                points_above = wall_above.all_walls_at_point_1()
+                for point_to_move in points_above:
+                    vertex_moves.append((point_to_move, new_position + offset.xy))
+                sector_above = sector_above.sector_above_ceiling
 
-            repeat_adjust_previous_point.adjust()
-            repeat_adjust.adjust()
+            sector_below = point_sector.sector_below_floor
+            wall_below = point
+            while sector_below is not None:
+                offset = sector_below.get_below_draw_offset()
+                wall_below = sector_below.find_wall_on_point(wall_below.point_1 + offset.xy)
+                points_below = wall_below.all_walls_at_point_1()
+                for point_to_move in points_below:
+                    vertex_moves.append((point_to_move, new_position + offset.xy))
+                sector_below = sector_below.sector_below_floor
+
+            for move_point, new_position in vertex_moves:
+                WallMove._do_move_vertex(move_point, new_position)
+
+    @staticmethod
+    def _do_move_vertex(point: map_objects.EditorWall, new_position: core.Point2):
+        repeat_adjust_previous_point = wall_repeat_adjust.WallRepeatAdjust(
+            point.wall_previous_point
+        )
+        repeat_adjust = wall_repeat_adjust.WallRepeatAdjust(point)
+
+        point.teleport_point_1_to(new_position)
+
+        repeat_adjust_previous_point.adjust()
+        repeat_adjust.adjust()
