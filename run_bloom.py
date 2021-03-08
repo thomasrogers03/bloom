c3766bb99887f3856e456d7a4d6cc3319696014d
Thomas Rogers <thomasrogers03@gmail.com>
2021-03-08 18:03:36 -0500

    save crash log

--- run_bloom.py
+++ run_bloom.py
@@ -1,11 +1,17 @@
 # Copyright 2020 Thomas Rogers
 # SPDX-License-Identifier: Apache-2.0
 
+import traceback
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         from bloom.main import main
 
         main()
     except Exception as error:
-        print(f'Unable to run Bloom due to {error}')
+        print(
+            f"Unable to run Bloom due to {error}, saving crash information to crash.log"
+        )
+        with open("crash.log", "w+") as file:
+            file.write(str(error))
+            file.write(traceback.format_exc())
